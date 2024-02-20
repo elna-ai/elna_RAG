62aa12124fde925b44eecc7b2630cb2a473290b1
Alex Jolly <alexjolly28@gmail.com>
2024-02-21 02:30:02 +0530

    vextordb query added

--- src/elna_RAG_backend/src/lib.rs
+++ src/elna_RAG_backend/src/lib.rs
@@ -1,18 +1,43 @@
 
-use ic_cdk::{update,export_candid};
+use ic_cdk::api::call::RejectionCode;
+use ic_cdk::{export_candid, query, update};
 use candid::Principal;
 use ic_cdk::api::call::CallResult;
 
-type FooResult = String;
+type GreetResult = String;
+type QueryResult = String;
+
 
 
 #[update]
-async fn make_inter_canister_call()-> CallResult<(FooResult,)>{
-    let word: String = "apple".into();
-    let result:  CallResult<(FooResult,)> = ic_cdk::call(Principal::from_text("br5f7-7uaaa-aaaaa-qaaca-cai").unwrap(), "greet", (word,)).await;
+async fn make_inter_canister_call(name:String)-> CallResult<(GreetResult,)>{
+    // let word: String = "apple".to_string();
+    let result:  CallResult<(GreetResult,)> = ic_cdk::call(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "greet", (name,)).await;
     result
-
 }
+
+
+#[update]
+async fn query(index_name: String, q: Vec<f32>, limit: i32)->Result<String,(RejectionCode,String)>{
+    // let word: String = "apple".to_string();
+    let result:  CallResult<(QueryResult,)> = ic_cdk::call(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "query", (index_name,q,limit)).await;
+    // result
+
+    // Handle the result
+    match result {
+        Ok(response) => {
+            // println!("Inter-canister call successful: {}", response);
+            Ok(format!("{}",response.0))
+        }
+        Err(err) => {
+            // eprintln!("Error making inter-canister call: {}", err);
+            Err(err)
+        }
+    }
+    
+}
+
+
 
 export_candid!();
 
--- src/test_canister/src/lib.rs
+++ src/test_canister/src/lib.rs
@@ -5,4 +5,26 @@ fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
 
+
+#[query]
+fn query(name: String, q: Vec<f32>, limit: i32) -> String {
+    // Placeholder implementation for demonstration
+    println!("Querying for {} with parameters {:?} and limit {}", name, q, limit);
+
+    // Dummy results, you would replace this with actual querying logic
+    let results: Vec<String> = vec![
+        "Result 1".to_string(),
+        "Result 2".to_string(),
+        "Result 3".to_string(),
+    ];
+
+    // Joining the results with newlines
+    let joined_results = results.join("\n");
+
+    // Returning the joined results
+    joined_results
+    
+    // Returning the results
+}
+
 export_candid!();
