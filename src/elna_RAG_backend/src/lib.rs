cbcd497e42b8743704f0f2c62c4bbab1226659a9
Alex Jolly <alexjolly28@gmail.com>
2024-02-21 00:58:33 +0530

    tested ok

--- src/elna_RAG_backend/src/lib.rs
+++ src/elna_RAG_backend/src/lib.rs
@@ -1,4 +1,18 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+
+use ic_cdk::{update,export_candid};
+use candid::Principal;
+use ic_cdk::api::call::CallResult;
+
+type FooResult = String;
+
+
+#[update]
+async fn make_inter_canister_call()-> CallResult<(FooResult,)>{
+    let word: String = "apple".into();
+    let result:  CallResult<(FooResult,)> = ic_cdk::call(Principal::from_text("br5f7-7uaaa-aaaaa-qaaca-cai").unwrap(), "greet", (word,)).await;
+    result
+
 }
+
+export_candid!();
+
---
+++ src/test_canister/src/lib.rs
@@ -0,0 +1,8 @@
+use ic_cdk::{query,export_candid};
+
+#[query]
+fn greet(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
+export_candid!();
